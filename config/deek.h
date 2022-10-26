679583701c72b4f2ef197a81c11b86a479b64d0f
Deek <deekcrypto@gmail.com>
2022-10-26 01:55:05 -0600

    Changed to my keymap

---
+++ config/deek.h
@@ -0,0 +1,31 @@
+#define _BASE 0
+#define _NUMB 1
+#define _FUNC 2
+#define _NAVI 3
+#define _SYST 4
+
+#define NAV_ENT _NAVI ENTER
+#define NUM_BCK _NUMB BACKSPACE
+#define NAV_BCK _NAVI BACKSPACE
+#define NUM_SPC _NUMB SPACE
+
+#define X_SELA LC(A)
+#define X_UNDO LC(Z)
+#define X_CUT LC(X)
+#define X_COPY LC(C)
+#define X_PAST LC(V)
+#define X_REDO LC(Y)
+#define HYPER LC(LS(LG(LALT)))
+#define MEH LC(LS(LALT))
+#define CMDTAB LG(TAB)
+#define CTRLTAB LC(TAB)
+#define X_A LCTRL A
+#define X_R LALT R
+#define X_S LGUI S
+#define X_T LSHFT T
+#define X_N RSHFT N
+#define X_E RGUI E
+#define X_I LALT I
+#define X_O RCTRL O
+
+#define C_D LCTRL DOWN
